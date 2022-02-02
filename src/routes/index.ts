69a1ace8a159b3f21c08003895ed97aa0fc672e5
Rafael <rafaelmota622@gmail.com>
2022-02-02 12:35:38 -0300

    Rotas iniciais

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,15 @@
 import { Router } from "express";
 
+import * as PageController from "../controllers/pageControllers";
+import * as SearchCrontroller from "../controllers/searchController";
+
 const router = Router();
 
-router.get("/", (req, res) => {
-  res.send("Olá mundo");
-});
+router.get("/", PageController.home);
+router.get("/dogs", PageController.dogs);
+router.get("/cats", PageController.cats);
+router.get("/fishes", PageController.fishes);
+
+router.get("/search", SearchCrontroller.search);
 
 export default router;
